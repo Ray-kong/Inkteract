f82b1b89bf9b24418ad39218ba45606e3cd392a6
RayKong <ray.hyun.kong@gmail.com>
2024-02-03 16:53:46 -0500

    dump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -16,7 +16,8 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         _body.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * _speed, _body.velocity.y);
-
+        
+        //TODO: improve movement. make jump less floaty and make it smoother
         if (Input.GetKey(KeyCode.Space) && _canJump)
         {
             _body.velocity = new Vector2(_body.velocity.x, _jumpForce);
