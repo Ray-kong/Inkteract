417fe6a16189386731fc6871ee0492a0a9c4df44
nicop <proskauer.n@northeastern.edu>
2024-04-16 14:26:37 -0400

    custom cursor

---
+++ Assets/Scripts/CurstomCursor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurstomCursor : MonoBehaviour
+{
+    RectTransform canvasRect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        canvasRect = GetComponentInParent<Canvas>().GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Input.mousePosition;
+
+        // Convert mouse position to Canvas space
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, mousePosition, null, out Vector2 localPoint);
+
+        // Set the position of the custom cursor to the converted position within the Canvas
+        transform.localPosition = localPoint;
+    }
+}
